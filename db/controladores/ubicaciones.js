c17dc1647d8eac5ae2c96fa612ff036225009484
Mario González <mario@mariogl.com>
2021-04-28 22:05:01 +0200

    Añadir ubicación y filtrado de ubicaciones por vehículo

--- db/controladores/ubicaciones.js
+++ db/controladores/ubicaciones.js
@@ -1,13 +1,38 @@
 const { creaRespuesta } = require(".");
+const { creaError } = require("../../server/errores");
 const Ubicacion = require("../modelos/ubicacion");
+const Usuario = require("../modelos/usuario");
+const Vehiculo = require("../modelos/vehiculo");
 
-const getUbicaciones = async (idUsuario) => {
-  const ubicaciones = await Ubicacion.find({
-    usuario: idUsuario,
-  }).sort({ fecha: 1 });
+const getUbicaciones = async (idUsuario, idVehiculo) => {
+  const ubicaciones = await Ubicacion.find(
+    {
+      usuario: idUsuario,
+      vehiculo: idVehiculo,
+    },
+    "-vehiculo -usuario -__v"
+  ).sort({ fecha: 1 });
   return creaRespuesta(null, ubicaciones);
+};
+
+const crearUbicacion = async (nuevaUbicacion) => {
+  const usuarioExiste = await Usuario.findById(nuevaUbicacion.usuario);
+  if (!usuarioExiste) {
+    return creaRespuesta(creaError("No existe el usuario", 404));
+  }
+  const vehiculoExiste = await Vehiculo.findById(nuevaUbicacion.vehiculo);
+  if (!vehiculoExiste) {
+    return creaRespuesta(creaError("No existe el vehículo", 404));
+  }
+  nuevaUbicacion.fecha = new Date();
+  const ubicacionCreada = await Ubicacion.create(nuevaUbicacion);
+  return creaRespuesta(null, {
+    id: ubicacionCreada.id,
+    fecha: ubicacionCreada.fecha,
+  });
 };
 
 module.exports = {
   getUbicaciones,
+  crearUbicacion,
 };
--- server/rutas/ubicaciones.js
+++ server/rutas/ubicaciones.js
@@ -1,20 +1,28 @@
 const debug = require("debug")("DHA:rutas:ubicaciones");
 const express = require("express");
 const { checkSchema, param } = require("express-validator");
-const { checkBadRequest } = require("../errores");
+const { checkBadRequest, creaError } = require("../errores");
 const ubicacionSchema = require("../schemas/ubicaciones");
-const { getUbicaciones } = require("../../db/controladores/ubicaciones");
+const {
+  getUbicaciones,
+  crearUbicacion,
+} = require("../../db/controladores/ubicaciones");
 
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  const { error, datos } = await getUbicaciones(req.idUsuario);
-  if (error) {
-    return next(error);
-  } else {
-    res.json({ datos });
+router.get(
+  "/vehiculo/:id",
+  param("id", "Id incorrecta").isMongoId(),
+  checkBadRequest(debug),
+  async (req, res, next) => {
+    const { error, datos } = await getUbicaciones(req.idUsuario, req.params.id);
+    if (error) {
+      return next(error);
+    } else {
+      res.json({ datos });
+    }
   }
-});
+);
 
 router.get(
   "/:id",
@@ -27,7 +35,19 @@ router.post(
   "/",
   checkSchema(ubicacionSchema),
   checkBadRequest(debug),
-  (req, res, next) => {}
+  async (req, res, next) => {
+    const nuevaUbicacion = req.body;
+    const { idUsuario } = req;
+    if (nuevaUbicacion.usuario !== idUsuario) {
+      return next(creaError("Usuario incorrecto", 400));
+    }
+    const { error, datos } = await crearUbicacion(nuevaUbicacion);
+    if (error) {
+      return next(error);
+    } else {
+      res.json({ datos });
+    }
+  }
 );
 
 module.exports = router;
